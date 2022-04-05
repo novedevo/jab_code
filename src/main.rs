115d30f838f0d3fc400f7d6fd737d15e22b7f10f
Devon Sawatsky <novedevo@gmail.com>
2022-04-05 15:20:53 -0700

    initial scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
-    println!("Hello, world!");
+    let encode_string = "testing";
+
+    let jab = JABCode {
+        data: encode_string.to_string(),
+        depth: ColourCount::Four,
+        x: 1,
+        y: 1,
+        ecc: ErrorCorrectionLevel::Zero,
+    };
+}
+
+struct JABCode {
+    data: String,
+    depth: ColourCount,
+    x: usize,
+    y: usize,
+    ecc: ErrorCorrectionLevel,
+}
+
+enum ColourCount {
+    Four,
+    Eight,
+}
+
+enum ErrorCorrectionLevel {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+}
+
+enum FourColours {
+    C,
+    M,
+    Y,
+    K,
 }
